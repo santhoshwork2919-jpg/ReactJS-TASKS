cf40fdff78338015e6f9a60d91ace137b23cebef
santhosh <santhoshwork2919@gmail.com>
2026-05-16 20:53:01 +0530

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+
+function Footer(){
+
+    const style = {
+    backgroundColor : "red"
+    };
+return(
+<div>
+
+   <footer style={style}>
+   <h1>  @Since2002 .Welcome to SAN company</h1>
+   </footer>
+
+ </div>
+   )
+}
+
+export default Footer;
---
+++ src/Sancom.jsx
@@ -0,0 +1,12 @@
+
+let a = 10 ;
+let b = 20 ;
+
+function Sancom(){
+    return(
+        <h1>{a+b}</h1>
+    );
+
+}
+
+export default Sancom;
